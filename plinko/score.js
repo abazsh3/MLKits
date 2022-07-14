510e90075ff47d51bcd01abaabda48f681105878
abazsh3 <abazsh3@gmail.com.com>
2022-07-14 18:06:21 +0430

    gather data  onScoreUpdate

--- plinko/score.js
+++ plinko/score.js
@@ -1,8 +1,9 @@
+const outPuts = [];
+
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
-  // Ran every time a balls drops into a bucket
+  outPuts.push([dropPosition, bounciness, size, bucketLabel]);
 }
 
 function runAnalysis() {
   // Write code here to analyze stuff
 }
-
